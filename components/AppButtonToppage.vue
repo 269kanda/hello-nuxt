fb9338cca4d3ecf1899a0c5074de2ab51f6a37e4
kandaYusuke <y.kanda@newdaysys.co.jp>
2019-05-28 13:36:06 +0900

    テスト

---
+++ components/AppButtonToppage.vue
@@ -0,0 +1,71 @@
+<template>
+<div>
+    <transition name='pagetop-fade'>
+    <div class='pagetop' v-show="pagetopDisp" v-scroll="handleScroll">
+      <a href='#'>^</a>
+    </div>
+    
+    </transition>
+    
+</div>
+</template>
+
+<script>
+
+
+
+export default {
+  data() {
+    return{
+      pagetopDisp: true
+    }
+  },
+  methods: {
+    handleScroll: function(evt){
+      if(window.pageYOffset > 90){
+        this.pagetopDisp = true;
+      } else {
+        this.pagetopDisp = false;
+      }
+    }
+  }
+}
+
+</script>
+
+<style>
+
+.pagetop {
+  position: fixed;
+  display: block;
+  width: 50px;
+  height: 50px;
+  right: 10%;
+  bottom: 8%;
+  
+  border: 1px solid #35495e;
+  border-radius: 30px;
+  background-color: #35495e;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.pagetop-fade-enter-active, .pagetop-fade-leave-active {
+  transition: opacity .5s;
+}
+
+.pagetop-fade-enter, .pagetop-fade-leave-to {
+  opacity: 0;
+}
+
+.pagetop a {
+  font-size: 30px;
+  color: #fff;
+  text-decoration: none;
+}
+
+
+  
+</style>
